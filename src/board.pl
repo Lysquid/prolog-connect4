b6539f766c6bbd85862712593941e91a9f29452b
Gartox <ewan71230@gmail.com>
2023-11-28 10:32:29 +0100

    Simplify win condition

--- src/board.pl
+++ src/board.pl
@@ -2,9 +2,9 @@ example_board([
     e, e, e, e, e, e, e,
     e, e, e, e, x, e, e,
     e, e, x, x, o, e, e,
-    e, e, o, x, o, e, e,
+    e, e, x, x, o, e, e,
     e, x, o, o, o, e, e,
-    e, x, o, x, x, o, x
+    e, x, o, x, e, o, x
 ]).
 
 empty_board([
@@ -16,11 +16,15 @@ empty_board([
     e, e, e, e, e, e, e
 ]).
 
+in_board(C, L) :-
+    between(0, 6, C),
+    between(0, 5, L).
+    
 cell(B, C, L, M) :- 
     I is L * 7 + C,
     nth0(I, B, M).
 
-win(B, M) :- win2(B, M, 0, 0).
+win(B, M) :- setof([C, L], (in_board(C, L), win2(B, M, C, L)), _).
 
 win2(B, M, C, L) :- 
     C >= 0,
@@ -35,30 +39,11 @@ win2(B, M, C, L) :-
         win3(B, M, C, L, 0, 1, 1) 
     ), !.
 
-win2(B, M, C, L) :- 
-    C >= 0,
-    L >= 0,
-    C < 6,
-    L < 6,
-    NC is C + 1,
-    win2(B, M, NC, L), !.
-
-win2(B, M, C, L) :- 
-    C >= 0,
-    L >= 0,
-    C = 6,
-    L < 5,
-    NL is L + 1,
-    win2(B, M, 0, NL), !.
-
 win3(_, _, _, _, _, _, N) :- N >= 4.
 win3(B, M, C, L, DC, DL, N) :- 
     NC is C + DC,
     NL is L + DL,
-    NC >= 0,
-    NL >= 0,
-    NC < 7,
-    NL < 6,
+    in_board(NC, NL),
     cell(B, NC, NL, M),
     NN is N + 1,
     win3(B, M, NC, NL, DC, DL, NN), !
